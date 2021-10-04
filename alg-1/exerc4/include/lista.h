126faee83aadace999f82fc1faf74702516f4d6b
HeitorPupim <heitorpupimtoledo@gmail.com>
2021-10-04 17:50:51 -0300

    create folders

---
+++ alg-1/exerc4/include/lista.h
@@ -0,0 +1,21 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef int elem;
+typedef struct lista_st lista_t;
+
+lista_t *cria();
+void destroi(lista_t *lista);
+
+void insere(lista_t *lista, elem x);
+void remover(lista_t *lista, elem x);
+
+void imprimir(lista_t *lista);
+
+void rotacionar(lista_t *lista);
+int tamanho(lista_t *lista);
+
+#endif // LISTA_H
---
+++ alg-1/exerc4/source/lista.c
@@ -0,0 +1,140 @@
+#include <lista.h>
+
+typedef struct no_st no_t;
+
+struct no_st {
+    elem info;
+    no_t *prox;
+};
+
+struct lista_st {
+    no_t *ini, *fim;
+    int tam;
+};
+
+/* Aloca espaço para a lista e
+ * define os parâmetros iniciais.
+ */
+
+lista_t *cria() {
+    lista_t *lista = malloc(sizeof(lista_t));
+
+    lista->tam = 0;
+    lista->ini = NULL;
+    lista->fim = NULL;
+
+    return lista;
+}
+
+/* Desaloca o espaço reservado para cada nó
+ * da lista e o espaço da própria lista.
+ */
+void destroi(lista_t *lista) {
+    if (lista == NULL) return;
+    no_t *p = lista->ini;
+    while (p != NULL) {
+        lista->ini = p->prox;
+        free(p);
+        p = lista->ini;
+    }
+    free(lista);
+}
+
+/* Aloca espaço para um nó, seta os parâmetros
+ * desse nó e insere no final da lista, fazendo
+ * as devidas verificações.
+ * Incrementa o tamanho da lista ao inserir esse novo nó.
+ */
+void insere(lista_t *lista, elem x) {
+    if (lista == NULL) return;
+    
+    no_t *p = malloc(sizeof(no_t));
+    p->info = x;
+    p->prox = NULL;
+
+    if (lista->ini == NULL)
+        lista->ini = p;
+    else
+        lista->fim->prox = p;
+
+    lista->fim = p;
+    lista->tam++;
+}
+
+/* Remove um nó da lista, independentemente da posição
+ * dele na lista.
+ * Procura o primeiro nó que contém esse valor, libera a memória
+ * reservada para ele, e diminui o tamanho da lista.
+ */
+void remover(lista_t *lista, elem x) {
+    if (lista == NULL) return;
+    no_t *anterior = NULL;
+    no_t *p = lista->ini;
+
+    while (p != NULL) {
+        if (p->info == x) {
+            if (p == lista->ini) {
+                lista->ini = lista->ini->prox;
+            } else if (p == lista->fim) {
+                lista->fim = anterior;
+                lista->fim->prox = NULL;
+            } else {
+                anterior->prox = p->prox;
+            }
+            free(p);
+            lista->tam--;
+            
+            return; // Se removeu
+        } else {
+            anterior = p;
+            p = p->prox;
+        }
+    }
+}
+
+/* Retorna o tamanho da lista
+ */
+int tamanho(lista_t *lista) {
+    if (lista != NULL) return lista->tam;
+
+    return -1;
+}
+
+/* Imprime o conteúdo de todos os nós
+ * da lista.
+ */
+void imprimir(lista_t *lista) {
+    if (lista == NULL) return;
+    no_t *p = lista->ini;
+
+    while (p != NULL) {
+        printf("%d ", p->info);
+        p = p->prox;
+    }
+    printf("\n");
+}
+
+/* Rotaciona a lista em uma unidade para a direita
+ * Busca o penúltimo nó,
+ */
+//Ultimo aponta para o primeiro
+//Penultimo aponta pra null
+//Ultimo se torna o primeiro
+//penultimo se torna o ultimo
+void rotacionar(lista_t *lista) {
+    no_t *p = lista->ini;
+    no_t *penultimo = NULL;
+
+    while (p->prox != NULL) {
+        penultimo = p;
+        p = p->prox;
+    }
+
+    no_t *aux = lista->ini;
+
+    lista->ini = lista->fim; // Primeiro aponta para o último
+    lista->fim = penultimo; // Ultimo aponta para o penúltimo
+
+    lista->ini->prox = aux; // Primeiro nó aponta para o inicio anterior
+    lista->fim->prox = NULL; // último nó aponta para NULL
+}
